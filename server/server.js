d846c83d3b9bcd5f51ff7c2f893e784b86d415c8
Lacey <lacey.griffith@g.austincc.edu>
2021-07-06 12:16:21 -0500

    add apollo server, addUser, login

--- server/server.js
+++ server/server.js
@@ -1,10 +1,26 @@
 const express = require('express');
 const path = require('path');
+
+//import Apollo server
+const { ApolloServer } = require('apollo-server-express')
+
+//import typeDefs and resolvers
+const { typeDefs, resolvers } = require('./schemas')
+
 const db = require('./config/connection');
-const routes = require('./routes');
+
+//import JWT middleware
+const { authMiddleware } = require('./utils/auth');
+// const routes = require('./routes');
 
 const app = express();
 const PORT = process.env.PORT || 3001;
+
+//new Apollo server
+const server = new ApolloServer({ typeDefs, resolvers, context: authMiddleware})
+
+// use express middleware
+server.applyMiddleware({ app })
 
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
@@ -14,7 +30,7 @@ if (process.env.NODE_ENV === 'production') {
   app.use(express.static(path.join(__dirname, '../client/build')));
 }
 
-app.use(routes);
+// app.use(routes);
 
 db.once('open', () => {
   app.listen(PORT, () => console.log(`🌍 Now listening on localhost:${PORT}`));
