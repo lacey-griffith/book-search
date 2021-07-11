abc1774f246b42b9118de5fe5d8954d8b4ae8987
Lacey <lacey.griffith@g.austincc.edu>
2021-07-10 23:22:19 -0500

    deploy to heroku

--- server/config/connection.js
+++ server/config/connection.js
@@ -5,6 +5,7 @@ mongoose.connect(process.env.MONGODB_URI || 'mongodb://localhost/googlebooks', {
   useUnifiedTopology: true,
   useCreateIndex: true,
   useFindAndModify: false,
-});
+}).then(() => console.log('Database connected successfully!'))
+.catch(err => console.log(err));
 
 module.exports = mongoose.connection;
